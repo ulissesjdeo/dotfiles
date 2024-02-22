a5b6e76cf337073460a091a2900f93c2c32c7492
Ulisses João de Oliveira <ulissesjoao2005@gmail.com>
2024-02-22 10:58:43 -0300

    Add PyCharm scripts

--- dot_config/scripts/executable_pycharm.sh
+++ dot_config/scripts/executable_pycharm.sh
@@ -12,3 +12,20 @@ sudo chown $USER:$USER pycharm-2020.3.5/
 sudo chmod 775 pycharm-2020.3.5/
 
 # Add to menu
+
+
+
+wget https://www.jetbrains.com/pycharm/download/download-thanks.html?type=eap,rc&platform=linux
+
+tar xzf pycharm-professional-*.tar.gz
+
+rm pycharm-professional-*.tar.gz
+
+sudo mv pycharm-*/ /opt/
+
+cd /opt/
+
+sudo chown $USER:$USER pycharm-*/
+sudo chmod 775 pycharm-*/
+
+# Add to menu
