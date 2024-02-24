736ead76e5b865df3e671a96582d93ccdda858f9
Ulisses João de Oliveira <ulissesjoao2005@gmail.com>
2024-02-24 15:59:25 -0300

    Rust tools

--- dot_config/scripts/cargo.sh
+++ dot_config/scripts/cargo.sh
@@ -1,5 +1,5 @@
 # On Arch systems
-#sudo pacman -S tealdeer tree most duf fd sccache eza bat ripgrep dust
+#sudo pacman -S tealdeer tree most duf fd sccache eza bat ripgrep dust procs bottom broot tokei fclones htmlq legdur lemmeknow just rargs pastel macchina ouch teehee kibi
 
 # On Windows systems
 cargo install tealdeer
@@ -11,3 +11,18 @@ cargo install eza
 cargo install bat
 cargo install ripgrep
 cargo install du-dust
+cargo install procs
+cargo install bottom
+cargo install broot
+cargo install tokei
+cargo install fclones
+cargo install htmlq
+cargo install legdur
+cargo install lemmeknow
+cargo install just
+cargo install rargs
+cargo install pastel
+cargo install macchina
+cargo install ouch
+cargo install teehee
+cargo install kibi
