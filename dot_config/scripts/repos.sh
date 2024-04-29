84994f290d6b2472a56f37614b5d72cae951819a
u <ulissesjoao2005@gmail.com>
2024-04-28 21:57:12 -0300

    Delete dot_config/scripts/repos.sh

--- dot_config/scripts/repos.sh
+++
@@ -1,7 +0,0 @@
-# Requires chezmoi init before
-gc git@github.com:ulissesjdeo/obsat.git
-gc git@github.com:ulissesjdeo/yoork.git
-gc git@github.com:ulissesjdeo/electroncounter.git
-gc git@github.com:ulissesjdeo/digitsadder.git
-gc git@github.com:ulissesjdeo/wincleaner.git
-gc git@github.com:ulissesjdeo/gymman.git
