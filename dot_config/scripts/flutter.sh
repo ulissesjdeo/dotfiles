1c25b889a71222447218a2448042ac4fceeb8611
u <ulissesjoao2005@gmail.com>
2024-04-28 21:57:26 -0300

    Delete dot_config/scripts/flutter.sh

--- dot_config/scripts/flutter.sh
+++
@@ -1,2 +0,0 @@
-paru -S --noconfirm flutter cmake ninja clang android-tools android-sdk-cmdline-tools-latest android-sdk-build-tools android-sdk-platform-tools android-platform android-studio libdbi-drivers sqlite libspatialite
-yes | flutter doctor --android-licenses
