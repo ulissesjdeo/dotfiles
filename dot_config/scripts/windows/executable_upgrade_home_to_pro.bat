6b0f6f65006276539ac316adf5db1a7547f2f27d
Ulisses João de Oliveira <ulissesjoao2005@gmail.com>
2024-05-07 22:29:20 -0300

    upgrade home to pro script fixed

--- dot_config/scripts/windows/executable_upgrade_home_to_pro.bat
+++ dot_config/scripts/windows/executable_upgrade_home_to_pro.bat
@@ -1,6 +1,3 @@
-slmgr.vbs /upk
-slmgr.vbs /ckms
 sc config LicenseManager start= auto & net start LicenseManager
 sc config wuauserv start= auto & net start wuauserv
 changepk.exe /productkey VK7JG-NPHTM-C97JM-9MPGT-3V66T
-exit
