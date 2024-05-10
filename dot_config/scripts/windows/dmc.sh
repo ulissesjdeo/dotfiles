166208696fc67d4cd1477286f6a1b591d24512dc
Ulisses João de Oliveira <ulissesjoao2005@gmail.com>
2024-05-10 11:12:44 -0300

    dmc script

--- dot_config/scripts/windows/dmc.sh
+++ dot_config/scripts/windows/dmc.sh
@@ -1,4 +1,7 @@
 cd $HOME
-export dmc_sav=`date +%d_%m_%Y_%H_%M_%S`
-7z a -mx9 $dmc_sav.zip /c/Program\ Files\ \(x86\)/Steam/userdata/318452227/631510/
-mv $dmc_sav.zip OneDrive/Documentos/saves/dmc
+
+export FILENAME=`date +%d_%m_%Y_%H_%M_%S`
+
+7z a -mx9 $FILENAME.zip /c/Program\ Files\ \(x86\)/Steam/userdata/318452227/631510/
+
+mv $FILENAME.zip OneDrive/Documentos/saves/dmc
