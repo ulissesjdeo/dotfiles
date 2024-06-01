ba251ec165d732bc65aa61684e31a02816b86746
u <ulissesjoao2005@gmail.com>
2024-06-01 18:09:28 -0300

    Update arch_miner.sh

--- dot_config/scripts/os/arch_miner.sh
+++ dot_config/scripts/os/arch_miner.sh
@@ -12,7 +12,8 @@ mount /dev/nvme0nXp1 /mnt/boot --mkdir
 # /etc/pacman.conf
 ParallelDownloads = 3
 
-pacstrap -K /mnt base base-devel linux linux-firmware amd-ucode nvidia nano sudo tmux wget git
+# If NVIDIA GPU add "nvidia"
+pacstrap -K /mnt base base-devel linux linux-firmware amd-ucode nano sudo tmux wget git
 
 # /mnt/etc/mkinitcpio.conf
 MODULES=(nvme nvme_core f2fs)
@@ -45,15 +46,27 @@ ParallelDownloads = 3
 %wheel ALL=(ALL:ALL) NOPASSWD: ALL
 
 useradd -mG wheel u
+usermod -s /bin/sh u
 usermod -s /bin/sh root
 passwd u
 passwd
 
-pacman -S networkmanager cronie openssh
-systemctl enable NetworkManager
+pacman -S dropbear dhcpcd cronie iwd
+systemctl enable dropbear
+systemctl enable dhcpcd
 systemctl enable cronie
-systemctl enable sshd
+systemctl enable iwd
 
+# efiboot
+pacman -S efibootmgr
+efibootmgr --create --disk /dev/nvme0n1 --part 1 --label "Arch Linux" \
+--loader /vmlinuz-linux --unicode 'root=/dev/nvme0n1p2 rw
+quiet nowatchdog nmi_watchdog=0 mitigations=off loglevel=0 rd.udev.log_level=0 \
+intel_idle.max_cstate=1 cryptomgr.notests initcall_debug intel_iommu=igfx_off \
+no_timer_check noreplace-smp page_alloc.shuffle=1 rcupdate.rcu_expedited=1 tsc=reliable
+initrd=\amd-ucode.img initrd=\initramfs-linux.img'
+
+# systemd-boot (for dualboot with Windows)
 bootctl install
 
 # /boot/loader/loader.conf
