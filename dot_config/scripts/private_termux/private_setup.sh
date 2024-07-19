1fc8c87364f18cad599632ef279a4d7e585407f8
Ulisses João de Oliveira <ulissesjoao2005@gmail.com>
2024-07-18 21:51:21 -0300

    Stop using sccache; Adding pnpm to Termux

--- dot_config/scripts/private_termux/private_setup.sh
+++ dot_config/scripts/private_termux/private_setup.sh
@@ -1,5 +1,6 @@
-pkg install eza bat zsh build-essential dart golang nodejs php python rust ffmpeg radare2 vim neovim dcraw imagemagick openjpeg-tools optipng
+pkg install eza bat zsh build-essential dart golang nodejs php python rust rustc-dev ffmpeg radare2 vim neovim dcraw imagemagick openjpeg-tools optipng ripgrep-all p7zip sqlite wget which xz-utils
 rm music movies podcasts documents audiobooks media-0 external-0 shared dcim downloads pictures
 rmdir storage
 ln -s /storage/emulated/0/ storage
 sh .config/scripts/zsh.sh
+npm install -g pnpm
