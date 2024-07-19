d219ae53dbb09f8e18e2c774261ad36ef801391e
Ulisses João de Oliveira <ulissesjoao2005@gmail.com>
2024-07-18 22:22:43 -0300

    add binutils

--- dot_config/scripts/private_termux/private_setup.sh
+++ dot_config/scripts/private_termux/private_setup.sh
@@ -1,4 +1,4 @@
-pkg install eza bat zsh build-essential dart golang nodejs php python rust rustc-dev ffmpeg radare2 vim neovim dcraw imagemagick openjpeg-tools optipng ripgrep-all p7zip sqlite wget which xz-utils busybox
+pkg install eza bat zsh build-essential dart golang nodejs php python rust rustc-dev ffmpeg radare2 vim neovim dcraw imagemagick openjpeg-tools optipng ripgrep-all p7zip sqlite wget which xz-utils busybox binutils
 rm music movies podcasts documents audiobooks media-0 external-0 shared dcim downloads pictures
 rmdir storage
 ln -s /storage/emulated/0/ storage
