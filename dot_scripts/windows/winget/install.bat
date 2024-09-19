361818789ee682e6307de3940e51a164bf8c01ae
u <ulissesjoao2005@gmail.com>
2024-09-19 03:23:36 +0000

    Moving scripts folder from .config/scripts to .scripts; Creating winget scripts

---
+++ dot_scripts/windows/winget/install.bat
@@ -0,0 +1,27 @@
+winget install --accept-package-agreements -e --id 7zip.7zip
+winget install --accept-package-agreements -e --id Anki.Anki
+winget install --accept-package-agreements -e --id Armin2208.WindowsAutoNightMode
+winget install --accept-package-agreements -e --id Balena.Etcher
+winget install --accept-package-agreements -e --id Discord.Discord
+winget install --accept-package-agreements -e --id Git.Git
+winget install --accept-package-agreements -e --id Skillbrains.Lightshot
+winget install --accept-package-agreements -e --id Obsidian.Obsidian
+winget install --accept-package-agreements -e --id Parsec.Parsec
+winget install --accept-package-agreements -e --id qBittorrent.qBittorrent
+winget install --accept-package-agreements -e --id Spotify.Spotify
+winget install --accept-package-agreements -e --id eloston.ungoogled-chromium
+winget install --accept-package-agreements -e --id VideoLAN.VLC
+winget install --accept-package-agreements -e --id Microsoft.WindowsTerminal.Preview
+winget install --accept-package-agreements -e --id Google.Chrome.Beta
+winget install --accept-package-agreements -e --id Postman.Postman
+winget install --accept-package-agreements -e --id JetBrains.PyCharm.Professional
+winget install --accept-package-agreements -e --id WinSCP.WinSCP
+winget install --accept-package-agreements -e --id GNU.Wget2
+winget install --accept-package-agreements -e --id JernejSimoncic.Wget
+winget install --accept-package-agreements -e --id WinDirStat.WinDirStat
+winget install --accept-package-agreements -e --id TorProject.TorBrowser
+winget install --accept-package-agreements -e --id Mozilla.Firefox.DeveloperEdition
+winget install --accept-package-agreements -e --id WinMerge.WinMerge
+winget install --accept-package-agreements -e --id Neovim.Neovim
+
+PAUSE
