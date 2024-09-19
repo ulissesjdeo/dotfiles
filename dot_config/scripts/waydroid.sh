c400121906fb44f2db812cf5ee1ccdb8a43b231b
u <ulissesjoao2005@gmail.com>
2024-09-19 02:17:00 +0000

    Codespaces changes for cleaner codebase

--- dot_config/scripts/waydroid.sh
+++
@@ -1,8 +0,0 @@
-paru -S --noconfirm waydroid
-
-sudo waydroid init -s GAPPS
-
-sudo systemctl disable waydroid-container
-
-# https://docs.waydro.id/faq/google-play-certification
-# https://docs.waydro.id/faq/using-adb-with-waydroid
