ef0a7e9d5b8808dc71bfbcc260e9c1c298a6a1aa
u <ulissesjoao2005@gmail.com>
2024-11-18 15:43:50 -0300

    Add eza

--- dot_scripts/arch.sh
+++ dot_scripts/arch.sh
@@ -82,7 +82,7 @@ AllowHibernation=yes
 AllowSuspendThenHibernate=no
 AllowHybridSleep=no
 
-pacman -S plasma-desktop dolphin konsole kscreen plasma-nm bluedevil power-profile-daemon plasma-pa gdisk dosfstools base-devel openssh
+pacman -S plasma-desktop dolphin konsole kscreen plasma-nm bluedevil power-profile-daemon plasma-pa gdisk dosfstools base-devel openssh eza
 
 exit
 umount -R /mnt
