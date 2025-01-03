44b610fc8a869ddaac58c9b1924880e2c54d1d9d
u <ulissesjoao2005@gmail.com>
2025-01-03 11:55:53 -0300

    Fix missing mdadm

--- dot_scripts/arch_desktop_raid.sh
+++ dot_scripts/arch_desktop_raid.sh
@@ -8,6 +8,7 @@ gdisk /dev/sdb
 
 mdadm --create --verbose --level=0 --metadata=1.2 --raid-devices=2 /dev/md/raid /dev/sda1 /dev/sda2
 cat /proc/mdstat
+mdadm --detail --scan >> /etc/mdadm.conf
 
 mkfs.fat -F 32 /dev/sda1
 mkfs.ext4 -v -L raidarray -b 4096 -E stride=128,stripe-width=256 /dev/md127
